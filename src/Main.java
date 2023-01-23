138012bb35ed530e83a16ec26625f165f8eeafcb
Jakub Toman <JakubT96@seznam.cz>
2023-01-23 16:10:48 +0100

    Other adjustments     -method     -conditions     -changing the file name

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 public class Main {
 
 
+    public static final String VAT_OVER = "vat-over-";
+    public static final String TXT = ".txt";
+
     public static void main(String[] args) throws IOException {
 
        Countryies register = new Countryies();
@@ -13,7 +16,7 @@ public class Main {
        }
         Countryies.enterOrValueFromUser();
         Countryies.processingData();
-        register.writeCountryToFile("vat-over-"+Countryies.numberFromUser +".txt");
+        register.writeCountryToFile(VAT_OVER +Countryies.numberFromUser + TXT);
     }
 }
 
